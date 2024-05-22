4853f3ba7cffc35d97509b3dff0e057b60c4e138
Flatter88 <163520885+Flatter88@users.noreply.github.com>
2024-05-23 01:59:51 +0300

    Update 6.py

--- 6/6.py
+++ 6/6.py
@@ -1,19 +1,10 @@
-C = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+import random
 
-chetniy = 0
-nechetniy = 0
+m = 15
+A = [random.randint(1, 100) for _ in range(m)] # - _ это не важное имя переменной
+B = [random.randint(1, 100) for _ in range(m)]
+K = [1 if A[i] > B[i] else 0 for i in range(m)]
 
-for i, element in enumerate(C):
-        if i % 2 == 0:
-                chetniy += element
-        else:
-                nechetniy += element
-print('Массив С:', C)
-
-print("Сумма четных :", chetniy)
-print("Сумма нечетных:", nechetniy)
-
-if chetniy > nechetniy:
-        print("Большая сумма: sum_even_index =", chetniy)
-else:
-        print("Большая сумма: sum_odd_index =", nechetniy)
+print("Массив A:", A)
+print("Массив B:", B)
+print("Массив K:", K)
